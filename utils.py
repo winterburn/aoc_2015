e0810a2ed50211da568eb5bfcba28bfe465a2f65
Olli Helminen <opadele@gmail.com>
2020-12-04 15:44:54 +0200

    little modification to utils

--- utils.py
+++ utils.py
@@ -5,4 +5,4 @@ def get_input(day):
         config = json.load(f)
     input = requests.get(f'https://adventofcode.com/2015/day/{day}/input',
                          cookies={'session': config.get('session')})
-    return input.text
+    return input.text.split('\n')
