752ffecbca002454307d07c19f232a322dd91280
Olli Helminen <opadele@gmail.com>
2020-12-04 15:29:44 +0200

    utils for fetching input

---
+++ utils.py
@@ -0,0 +1,8 @@
+import requests
+import json
+def get_input(day):
+    with open('./config.json', 'r') as f:
+        config = json.load(f)
+    input = requests.get(f'https://adventofcode.com/2015/day/{day}/input',
+                         cookies={'session': config.get('session')})
+    return input.content
